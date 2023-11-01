ed820d823ddaaff603cc8fa23f1caef61c9f8d4d
chandu <chandu.christ@gmail.com>
2023-11-01 17:15:06 +0530

    contact loc

--- lib/contact.dart
+++ lib/contact.dart
@@ -14,171 +14,175 @@ class ContactScreen extends StatelessWidget {
         title: const Text('Contact Us'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 28.0),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                InkWell(
-                  onTap: () async {
-                    final Uri url = Uri(scheme: 'tel', path: "9666534389");
-                    if (await canLaunchUrl(url)) {
-                      await launchUrl(url);
-                    } else {
-                      print('cannot launch this url');
-                    }
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade600,
-                          blurRadius: 10,
-                          spreadRadius: 1,
-                          offset: const Offset(4, 4),
-                        ),
-                        const BoxShadow(
-                          color: Colors.white,
-                          blurRadius: 10,
-                          spreadRadius: 1,
-                          offset: Offset(-4, -4),
-                        ),
-                      ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              InkWell(
+                onTap: () async {
+                  final Uri url = Uri(scheme: 'tel', path: "9666534389");
+                  if (await canLaunchUrl(url)) {
+                    await launchUrl(url);
+                  } else {
+                    print('cannot launch this url');
+                  }
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade600,
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                        offset: const Offset(4, 4),
+                      ),
+                      const BoxShadow(
+                        color: Colors.white,
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                        offset: Offset(-4, -4),
+                      ),
+                    ],
+                  ),
+                  width: 200,
+                  child: Lottie.asset("animations/phone.json"),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: Center(
+                  child: InkWell(
+                    onTap: () async {
+                      final Uri url = Uri(scheme: 'tel', path: "9666534389");
+                      if (await canLaunchUrl(url)) {
+                        await launchUrl(url);
+                      } else {
+                        print('cannot launch this url');
+                      }
+                    },
+                    child: Text(
+                      'For Prayer Requests plz call',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                    width: 200,
-                    child: Lottie.asset("animations/phone.json"),
                   ),
                 ),
-                SizedBox(
-                  height: 20,
-                ),
-                Container(
-                  child: Center(
-                    child: InkWell(
-                      onTap: () async {
-                        final Uri url = Uri(scheme: 'tel', path: "9666534389");
-                        if (await canLaunchUrl(url)) {
-                          await launchUrl(url);
-                        } else {
-                          print('cannot launch this url');
-                        }
+              ),
+              SizedBox(
+                height: 60,
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const MyMapPage();
                       },
-                      child: Text(
-                        'For Prayer Requests plz call',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
                     ),
+                  );
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.shade600,
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                        offset: const Offset(4, 4),
+                      ),
+                      const BoxShadow(
+                        color: Colors.white,
+                        blurRadius: 10,
+                        spreadRadius: 1,
+                        offset: Offset(-4, -4),
+                      ),
+                    ],
+                  ),
+                  width: 200,
+                  height: 100,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 16.0,
+                    ),
+                    child: Lottie.asset("animations/location.json"),
                   ),
                 ),
-                SizedBox(
-                  height: 60,
-                ),
-              ],
-            ),
-            Column(
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const MyMapPage();
-                        },
-                      ),
-                    );
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.shade600,
-                          blurRadius: 10,
-                          spreadRadius: 1,
-                          offset: const Offset(4, 4),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Container(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          'THANDRI SANNIDHI MINISTRIES',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
-                        const BoxShadow(
-                          color: Colors.white,
-                          blurRadius: 10,
-                          spreadRadius: 1,
-                          offset: Offset(-4, -4),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'CHILAKALURIPET,PALNADU DIST',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'PIN CODE-522616',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'ANDHRA PRADESH,INDIA',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '9666534389,9666774417',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
                       ],
                     ),
-                    width: 100,
-                    child: Lottie.asset("animations/loc.json"),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: Container(
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Text(
-                            'THANDRI SANNIDHI MINISTRIES',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'CHILAKALURIPET,PALNADU DIST',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'PIN CODE-522616',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            'ANDHRA PRADESH,INDIA',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '9666534389,9666774417',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
